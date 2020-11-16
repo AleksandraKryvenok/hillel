207e67842d887dd535a882322a0cd5a645400a27
AleksandraKryvenok <72933490+AleksandraKryvenok@users.noreply.github.com>
2020-11-16 19:05:23 +0200

    Add files via upload

---
+++ Accordion/scripts.js
@@ -0,0 +1,35 @@
+"use strict";
+
+function Accordion(element){
+    this.title = element.querySelector(".title");
+    this.body = element.querySelector(".body"); 
+
+    this.toggle = function(){
+        this.body.classList.toggle("hidden");
+    };
+    this.open = function(){
+        this.body.classList.remove("hidden");
+    };
+    this.close = function(){
+        this.body.classList.add("hidden");
+    };
+
+    //this.title.onclick = function(){console.log(this)};
+};
+
+const htmlAccordion = new Accordion(document.querySelector("#html"));
+const cssAccordion = new Accordion(document.querySelector("#css"));
+const jsAccordion = new Accordion(document.querySelector("#js"));
+
+const accordions = [htmlAccordion, cssAccordion, jsAccordion];
+
+//при открытии скрываем все body
+accordions.forEach(function(accordion){
+    accordion.close();  
+});
+
+
+
+
+
+
